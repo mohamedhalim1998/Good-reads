3a4b31430925b192bc74b86f3adaae57720fd087
MohamedHalim <mohamedhalim1998@gmail.com>
2023-02-28 13:52:05 +0200

    ADD: convert profile to user details to use in security context

--- src/main/java/com/mohamed/halim/goodreads/model/Profile.java
+++ src/main/java/com/mohamed/halim/goodreads/model/Profile.java
@@ -4,6 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.*;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
@@ -18,14 +22,34 @@ import java.util.List;
 public class Profile implements UserDetails {
     @Id
     private String username;
+    private String email;
     private String fullName;
     private String password;
     private Date birthdate;
     private String profilePic;
-    @ToString.Exclude
-    private List<Review> reviews;
-    @ToString.Exclude
-    private List<Book> bookRead;
-    @ToString.Exclude
-    private List<Shelf> shelves;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
