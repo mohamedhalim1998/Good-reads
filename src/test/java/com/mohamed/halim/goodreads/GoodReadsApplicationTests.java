493690fe15c80958a76385026143c40c1a139c24
MohamedHalim <mohamedhalim1998@gmail.com>
2023-03-06 08:45:27 +0200

    ADD: tests for security configurations

--- src/test/java/com/mohamed/halim/goodreads/GoodReadsApplicationTests.java
+++ src/test/java/com/mohamed/halim/goodreads/GoodReadsApplicationTests.java
@@ -2,8 +2,10 @@ package com.mohamed.halim.goodreads;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 class GoodReadsApplicationTests {
 
 	@Test
---
+++ src/test/java/com/mohamed/halim/goodreads/config/SecurityConfigurationTest.java
@@ -0,0 +1,38 @@
+package com.mohamed.halim.goodreads.config;
+
+import com.mohamed.halim.goodreads.model.dto.Login;
+import com.mohamed.halim.goodreads.model.dto.Registration;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+class SecurityConfigurationTest {
+    @Autowired
+    private WebTestClient webTestClient;
+
+
+    @Test
+    public void test_allowRegister() {
+        webTestClient.post().uri("/api/v1/auth/register").body(
+                Mono.just(new Registration("user1", "A@e,.com","password")), Registration.class
+        ).exchange().expectStatus().isOk();
+    }
+
+    @Test
+    public void test_allowLogin() {
+        webTestClient.post().uri("/api/v1/auth/login").body(
+                Mono.just(new Login("user1", "password")), Login.class
+        ).exchange().expectStatus().isOk();
+    }
+
+    @Test
+    public void test_checkJwtForAnyOther() {
+        webTestClient.get().uri("/api/v1/test").exchange().expectStatus().isUnauthorized();
+    }
+
+}
